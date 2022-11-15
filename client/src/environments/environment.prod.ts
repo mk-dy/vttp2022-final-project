67e8ddc033a0ce64f8ca63ea67657ddb351ba5a6
mk-dy <matthewkoh.dy@gmail.com>
2022-11-16 05:28:23 +0800

    done

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  stripePubKey: 'pk_test_51M1nVvEoamSM7sHHRbyyLYTemuXDdWnZbaNNiSsZtCN2y81jeekH7P4O7QLhSVwpCNmUFfjJYqaSxaGsSUJeD8O400UYqwk4Vm'
+  
 };
